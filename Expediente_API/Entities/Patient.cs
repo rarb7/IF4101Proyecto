5b8396fda2b61c24fd5b30798cbdecc80ffc022c
rarb7 <raalexa43@gmail.com>
2021-07-13 21:13:08 -0600

    subiendo API

---
+++ Expediente_API/Entities/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expediente_API.Entities
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+
+        public string PatientLastname { get; set; }
+        public int PatientAge { get; set; }
+        public string Password { get; set; }
+    }//fin clase Paciente
+}
